8029220ca0e4712b4b36a2788b1a3c7354de5ae7
Student <student@c02kw33hfh51.dalton.org>
2015-12-14 09:44:52 -0500

    redo

---
+++ Dropbox/Homework/workspace/WhatName/src/WhatsInAName.java
@@ -0,0 +1,73 @@
+import java.util.*;
+public class WhatsInAName {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner();
+		char[] input = scan.nextLine().toCharArray();
+
+	}
+	//marco
+	static char[] reverse 
+	{
+		return
+	}
+	//damon
+	static int vowel
+	{
+		//REAL ACTUAL CHANGES
+	}
+	//Nicky
+	static int consonentFrequency
+	{
+		
+	}
+	//Ryan
+	static String firtsName
+	{
+		
+	}
+	static string lastName
+	{
+		
+	}
+	static string middleName
+	{
+		
+	}
+	static boolean hyphen
+	{
+		
+	}
+	static char[] lowerCase
+	{
+		
+	}
+	static char[] upperCase
+	{
+		
+	}
+	static char[] random
+	{
+		
+	}
+	static boolean palindrome
+	{
+		
+	}
+	static char[] sorted
+	{
+		
+	}
+	static char[] initials
+	{
+		
+	}
+	static boolean title
+	{
+		
+	}
+	static char[] strongSyllable
+	{
+		
+	}
+
+}
